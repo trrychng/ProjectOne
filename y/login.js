fe5d9b4c75088a4ebd88a28f51ff21f07fc6b234
Rob <robert.koskulics@me.com>
2017-12-28 16:29:04 -0500

    Attempting to add Yelp

--- y/login.js
+++ y/login.js
@@ -27,6 +27,36 @@ function signOut(){
   loginWindowLoad();
   console.log("Signed out")
 };
+// Getting yelp API data
+// function yelpAPI(){ 
+//   var placeName = $(this).attr("data-name");
+//   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lzcGYta9bfMdDiwu3ZImzoJH3rgRatgC&q=" + animalName + "&limit=10&offset=0&&lang=en";
+//   $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).done(function(response) {
+//       for (var i = 0; i < response.data.length - 1; i++) {
+//         // Saves the URL for both still and moving GIFs into variables to use later
+//         var stillGif = response.data[i].images.fixed_height_still.url;
+//         var movingGif = response.data[i].images.fixed_height.url;
+//         // Saves the rating into a variable for later
+//         var rating = response.data[i].rating;
+//         // creates an image tag to put into the HTML
+//         var animalImage = $("<img>");
+//         //Adds the src attribute and url to the image tag, and alt text, and still state
+//         animalImage.attr("data-state", "still")
+//         animalImage.attr("data-still", stillGif)
+//         animalImage.attr("data-animate", movingGif)
+//           animalImage.attr("src", stillGif);
+//           animalImage.attr("alt", "animal image");
+//           animalImage.addClass("gifType");
+//           // Puts the GIF on the HTML
+//           $("#gifs").prepend(animalImage, "rated: " + rating);
+
+//         }
+      
+//     });
+// };
 function sendToEvents(){
   // Grabs the input the user typed in
   var nightOutName = $("#newNightOutInput").val().trim();
@@ -39,7 +69,17 @@ function sendToEvents(){
   // Clears the field 
   $("#newNightOutInput").val("");
 
+  // Gets Yelp data
+  var apiKey = "ziosgzuBXwFq0dTRSuehNLtM-7EKhlEK4hQP55I9bELwdHPDUvgMhwCLuZ494aI4UKqLDWI1MVz7QEDsucFMk4aKKYnU1urDddyU7kWDMXI7tehe_EOLQHT9fGBFWnYx";
+  var queryURL = "https://api.yelp.com/v3/businesses/search?api_key=" + apiKey + "=Starbucks";
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log("Queried Yelp");
+    });
 };
+
 // The action that signs in
 function signIn(){
   console.log("I clicked the login button");
